3caae97338c8e5d6ccd742bf31f6819cb1f2a1d0
Antone Gonzales <es.angelika@gmail.com>
2025-01-25 22:40:23 -0500

    feat: actualizar contenido dinámico del título y párrafo

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
-//variables
-let limiteMaximo = 100;
-let numeroSecreto = Math.floor(Math.random()*limiteMaximo)+1;
-let numeroUsuario = 0;
-let intentos = 1;
-let maximoIntentos = 5;
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto';
 
-while ( numeroUsuario != numeroSecreto ){
-    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${limiteMaximo} por favor:`));
-    console.log(typeof(numeroUsuario));
-
-    if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${ intentos ==1 ? 'vez' : 'veces'}`);
-    } else {
-        if (numeroUsuario > numeroSecreto ){
-            alert(`El numero secreto es menor`);
-        } else {
-            alert(`El numero secreto es mayor`);
-        }
-        intentos++;
-
-        if (intentos > maximoIntentos){
-            alert(`Haz alcanzado el máximo de ${maximoIntentos} intentos`)
-            break;
-        }
-    }
-}
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Elige un número del 1 al 10';
